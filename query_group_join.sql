65692677b23370180423a59d228929bbce5d3cbf
santocaridi99 <santocaridi99@gmail.com>
2022-02-23 17:43:15 +0100

    query con  operazione join

--- query_group_join.sql
+++ query_group_join.sql
@@ -1,3 +1,6 @@
+-- GROUP BY
+
+
 -- Contare quanti iscritti ci sono stati ogni anno
 -- -----------------------------------------------
 -- seleziono tutte le somme delle righe e degli anni (funzione year )
@@ -29,3 +32,98 @@ GROUP BY `exam_id`
 SELECT COUNT(*) , `department_id`
 FROM `degrees`
 GROUP BY `department_id`
+
+-- JOIN
+
+
+-- Selezionare tutti gli studenti iscritti al Corso di Laurea in Economia
+-- ---------------------------------------------------------
+-- seleziono le righe che voglio vedere , in questo caso ho messo id dello studente nome , cognome e corso di laurea
+-- prendo questi dati da studenti e da degrees facendo la operazione inner Join .
+-- messo condizione per visualizzare solo gli studenti con corso di laurea desiderato
+SELECT  `students`.`id`,`students`.`name`,`students`.`surname` ,`degrees`.`name`
+FROM `students` 
+JOIN `degrees`
+  ON `degrees`.`id`= `students`.`degree_id`
+WHERE `degrees`.`name` = 'Corso di Laurea in Economia';
+
+
+-- Selezionare tutti i Corsi di Laurea del Dipartimento di Neuroscienze
+-- -------------------------------------------------------------
+-- selezionato da degrees solo i nomi dei corsi  e del dipartimento 
+-- preso nome dal dipartimento facendo la inner join 
+-- messo condizione per visualizzare solo gli studenti con corso di laurea desiderato
+SELECT `degrees`.`name`,`departments`.`name` 
+FROM `degrees` 
+JOIN `departments`
+  ON `departments`.`id`= `degrees`.`department_id`
+WHERE `departments`.`name` = 'Dipartimento di Neuroscienze';
+
+
+-- Selezionare tutti i corsi in cui insegna Fulvio Amato (id=44)
+-- ---------------------------------------------------------
+--  join prima sulla tabella ponte
+-- dopo faccio un left join che restituisce i valori alla tabella di sinistra anche quelli non corrispondeti.
+-- e mettendo condizione che teacher id = 44 avrò i corsi in cui insegna quell'insegnante
+SELECT `teachers`.`id`, `teachers`.`name`,`teachers`.`surname`,`courses`.`name`
+FROM `teachers`
+ JOIN `course_teacher`
+   ON `course_teacher`.`teacher_id`= `teachers`.`id`
+LEFT JOIN `courses`
+       ON  `courses`.`id`= `course_teacher`.`course_id`      
+WHERE `teachers`.`id`= 44;
+
+
+-- Selezionare tutti gli studenti con i dati relativi al corso di laurea a cui sono iscritti e il
+-- relativo dipartimento, in ordine alfabetico per cognome e nome
+-- ----------------------------------------------------------------------
+-- stesso procedimento come sopra ,solo che faccio un order alla fine per cognome e nome
+SELECT `students`.`name`,`students`.`surname`,`degrees`.`name`,`departments`.`name`
+FROM `students`
+JOIN `degrees`
+	ON `degrees`.`id` = `students`.`degree_id`
+LEFT JOIN `departments`
+	ON `departments`.`id` = `degrees`.`department_id`
+ORDER BY `students`.`surname`, `students`.`name`;
+
+
+-- Selezionare tutti i corsi di laurea con i relativi corsi e insegnanti
+-- ---------------------------------------------------------------------
+SELECT `degrees`.`name` , `courses`.`name` ,`teachers`.`name`, `teachers`.`surname` 
+FROM `degrees`
+JOIN `courses`
+	ON `courses`.`degree_id` = `degrees`.`id`
+LEFT JOIN `course_teacher`
+	ON `course_teacher`.`course_id` = `courses`.`id`
+LEFT JOIN `teachers`
+	ON `teachers`.`id` = `course_teacher`.`teacher_id`
+
+
+-- Selezionare tutti i docenti che insegnano nel Dipartimento di Matematica (54)
+-- ------------------------------------------------------------------
+-- condizione che il dipartimento sia quello di matematica
+SELECT `departments`.`name` ,`teachers`.`name` , `teachers`.`surname`
+FROM `departments`
+JOIN `degrees`
+	ON `degrees`.`department_id` = `departments`.`id`
+JOIN `courses`
+	ON `courses`.`degree_id` = `degrees`.`id`
+JOIN `course_teacher`
+	ON `course_teacher`.`course_id` = `courses`.`id`
+JOIN `teachers`
+	ON `teachers`.`id` = `course_teacher`.`teacher_id`
+WHERE `departments`.`name`= 'Dipartimento di Matematica';
+
+-- BONUS: Selezionare per ogni studente quanti tentativi d’esame ha sostenuto per
+-- superare ciascuno dei suoi esame
+-- ----------------------------------------------------
+-- per i tentativi conto le righe e rinomino la tabella tentativi
+SELECT `students`.`id`, `students`.`name`, `students`.`surname`, `exams`.`course_id`, `courses`.`name`,COUNT(*) AS `attempts`
+FROM `students`
+JOIN `exam_student`
+	ON `students`.`id` = `exam_student`.`student_id`
+JOIN `exams`
+	ON `exam_student`.`exam_id` = `exams`.`id`
+JOIN `courses`
+	ON `exams`.`course_id` = `courses`.`id`
+GROUP BY `students`.`id`, `exams`.`course_id`
